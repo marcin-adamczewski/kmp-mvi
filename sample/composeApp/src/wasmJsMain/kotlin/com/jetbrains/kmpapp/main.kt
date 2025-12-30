2a3e398fea4d3155b0a85792bd23a48ae6fb9a8d
marcin-adamczewski <marcin.adamczewski@appunite.com>
2025-12-30 21:06:46 +0100

    Removed unused import

--- sample/composeApp/src/wasmJsMain/kotlin/com/jetbrains/kmpapp/main.kt
+++ sample/composeApp/src/wasmJsMain/kotlin/com/jetbrains/kmpapp/main.kt
@@ -3,7 +3,6 @@ package com.jetbrains.kmpapp
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.jetbrains.kmpapp.di.initKoin
-import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {

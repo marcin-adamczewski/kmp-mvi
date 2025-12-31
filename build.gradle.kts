864ff857aa1d0f303d0ed7b417fbdbc05bd6c515
marcin-adamczewski <marcin.adamczewski@appunite.com>
2025-12-31 16:48:57 +0100

    Extract version from local.properties to test on maven local

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,18 @@ plugins {
     alias(libs.plugins.android.lint) apply false
     alias(libs.plugins.junit5) apply false
 }
+
+val localProperties = java.util.Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { load(it) }
+    }
+}
+
+val localVersion = localProperties.getProperty("version") ?: localProperties.getProperty("VERSION_NAME")
+
+if (localVersion != null) {
+    allprojects {
+        version = localVersion
+    }
+}

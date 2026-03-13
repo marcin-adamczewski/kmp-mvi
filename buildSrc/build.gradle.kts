6e9883b65914cda799d51814e888f6dc6837029c
marcin-adamczewski <marcin.adamczewski@appunite.com>
2026-03-13 17:31:36 +0100

    Code cleanup

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
 dependencies {
     implementation(libs.android.gradle)
     implementation(libs.kotlin.gradle)
-    //compileOnly(libs.compose.gradlePlugin) // If using Compose Multiplatform
 }
